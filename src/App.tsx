1eadb5246fcaf230258950b6b12910eddb46be13
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-09 11:17:39 +0300

    input change event

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
 // function component oldugunu belirmek icin 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [workDay, setWorkDay] = useState<number>(0);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setWorkDay(Number(event.target.value));
+    }
+    console.log(task, workDay);
+  }
   return (
     <div className="App">
-
+      <input type='text' name='task' value={task} placeholder='taskınızı giriniz…' onChange={handleChange} />
+      <input type='number' name='workDay' value={workDay} placeholder='Kaç günde tamamlamalısınız' onChange={handleChange} />
+      <button >Yeni task ekle</button>
     </div>
   );
 }
